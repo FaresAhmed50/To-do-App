8309e90824ffbe448d08934927c3835f1c301433
Fares <kojim2023@gmail.com>
2024-10-10 00:23:41 +0300

    Creating the basic files

---
+++ template/TemplateName.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './TemplateName.module.css';
+
+const Templatename = () => {
+    return (
+        <div>
+            TemplateName component
+        </div>
+    );
+};
+
+export default Templatename;
